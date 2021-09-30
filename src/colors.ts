3a206df867c8ef155ecb883997ad0dafd321d82a
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2021-10-01 01:47:48 +0900

    feat: Add natural 6 colors

--- src/colors.ts
+++ src/colors.ts
@@ -5,6 +5,12 @@ import color0000FFPNG from "./color0000FF.png";
 import colorFFFF00PNG from "./colorFFFF00.png";
 import colorFF00FFPNG from "./colorFF00FF.png";
 import color00FFFFPNG from "./color00FFFF.png";
+import colorEF8F8FPNG from "./colorEF8F8F.png";
+import color8FEF8FPNG from "./color8FEF8F.png";
+import color8F8FEFPNG from "./color8F8FEF.png";
+import colorEFEF8FPNG from "./colorEFEF8F.png";
+import colorEF8FEFPNG from "./colorEF8FEF.png";
+import color8FEFEFPNG from "./color8FEFEF.png";
 import colorFFFFFFPNG from "./colorFFFFFF.png";
 
 const colors = {
@@ -41,6 +47,36 @@ const colors = {
   "#00ffff": {
     button: {
       image: color00FFFFPNG,
+    },
+  },
+  "#ef8f8f": {
+    button: {
+      image: colorEF8F8FPNG,
+    },
+  },
+  "#8fef8f": {
+    button: {
+      image: color8FEF8FPNG,
+    },
+  },
+  "#8f8fef": {
+    button: {
+      image: color8F8FEFPNG,
+    },
+  },
+  "#efef8f": {
+    button: {
+      image: colorEFEF8FPNG,
+    },
+  },
+  "#ef8fef": {
+    button: {
+      image: colorEF8FEFPNG,
+    },
+  },
+  "#8fefef": {
+    button: {
+      image: color8FEFEFPNG,
     },
   },
   "#ffffff": {
