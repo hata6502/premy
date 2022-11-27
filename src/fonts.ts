0aea629284b215882aba5ed877a8336eaf94b1e5
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2022-11-27 11:05:40 +0900

    feat: Remove noto emoji

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,14 +1,14 @@
 const fonts = {
   "sans-serif":
-    '"Noto Emoji", "Open Sans", "Fira Sans", "Lucida Sans", "Lucida Sans Unicode", "Trebuchet MS", "Liberation Sans", "Nimbus Sans L", sans-serif',
+    '"Open Sans", "Fira Sans", "Lucida Sans", "Lucida Sans Unicode", "Trebuchet MS", "Liberation Sans", "Nimbus Sans L", sans-serif',
   serif:
-    '"Noto Emoji", Lucida Bright, Lucida Fax, Palatino, "Palatino Linotype", Palladio, "URW Palladio", serif',
+    'Lucida Bright, Lucida Fax, Palatino, "Palatino Linotype", Palladio, "URW Palladio", serif',
   monospace:
-    '"Noto Emoji", "Fira Mono", "DejaVu Sans Mono", Menlo, Consolas, "Liberation Mono", Monaco, "Lucida Console", monospace',
+    '"Fira Mono", "DejaVu Sans Mono", Menlo, Consolas, "Liberation Mono", Monaco, "Lucida Console", monospace',
   cursive:
-    '"Noto Emoji", "Dancing Script", "Hachi Maru Pop", "Brush Script MT", "Brush Script Std", "Lucida Calligraphy", "Lucida Handwriting", "Apple Chancery", cursive',
+    '"Dancing Script", "Hachi Maru Pop", "Brush Script MT", "Brush Script Std", "Lucida Calligraphy", "Lucida Handwriting", "Apple Chancery", cursive',
   fantasy:
-    '"Noto Emoji", "Fruktur", "Potta One", Papyrus, Herculanum, Party LET, Curlz MT, Harrington, fantasy',
+    '"Fruktur", "Potta One", Papyrus, Herculanum, Party LET, Curlz MT, Harrington, fantasy',
 };
 
 type FontType = keyof typeof fonts;
