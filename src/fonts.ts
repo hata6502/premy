4661f2a44c54fa229fe5be6c9deed7e5c10fc1ae
Tomoyuki Hata <7702653+hata6502@users.noreply.github.com>
2023-02-28 00:53:06 +0900

    fix: Noto Color Emoji

--- src/fonts.ts
+++ src/fonts.ts
@@ -1,14 +1,14 @@
 const fonts = {
   "sans-serif":
-    '"Open Sans", "Fira Sans", "Lucida Sans", "Lucida Sans Unicode", "Trebuchet MS", "Liberation Sans", "Nimbus Sans L", sans-serif',
+    '"Noto Color Emoji", "Open Sans", "Fira Sans", "Lucida Sans", "Lucida Sans Unicode", "Trebuchet MS", "Liberation Sans", "Nimbus Sans L", sans-serif',
   serif:
-    'Lucida Bright, Lucida Fax, Palatino, "Palatino Linotype", Palladio, "URW Palladio", serif',
+    '"Noto Color Emoji", Lucida Bright, Lucida Fax, Palatino, "Palatino Linotype", Palladio, "URW Palladio", serif',
   monospace:
-    '"Fira Mono", "DejaVu Sans Mono", Menlo, Consolas, "Liberation Mono", Monaco, "Lucida Console", monospace',
+    '"Noto Color Emoji", "Fira Mono", "DejaVu Sans Mono", Menlo, Consolas, "Liberation Mono", Monaco, "Lucida Console", monospace',
   cursive:
-    '"Dancing Script", "Hachi Maru Pop", "Brush Script MT", "Brush Script Std", "Lucida Calligraphy", "Lucida Handwriting", "Apple Chancery", cursive',
+    '"Noto Color Emoji", "Dancing Script", "Hachi Maru Pop", "Brush Script MT", "Brush Script Std", "Lucida Calligraphy", "Lucida Handwriting", "Apple Chancery", cursive',
   fantasy:
-    '"Fruktur", "Potta One", Papyrus, Herculanum, Party LET, Curlz MT, Harrington, fantasy',
+    '"Noto Color Emoji", "Fruktur", "Potta One", Papyrus, Herculanum, Party LET, Curlz MT, Harrington, fantasy',
 };
 
 type FontType = keyof typeof fonts;
