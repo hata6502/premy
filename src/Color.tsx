76ecd046b7cd062155caa45db4e2f95a791b057b
hata6502 <7702653+hata6502@users.noreply.github.com>
2026-06-28 15:06:14 +0900

    fix: 白色の選択UIを見やすく

--- src/Color.tsx
+++ src/Color.tsx
@@ -1,8 +1,15 @@
 import { FunctionComponent, memo } from "react";
 
+const borderColor = "#bdbdbd";
+const borderTargetColors = ["#fff", "#ffffff", "#f5f5f5"];
+
 export const Color: FunctionComponent<{
   color: string;
 }> = memo(({ color }) => {
+  const stroke = borderTargetColors.includes(color.toLowerCase())
+    ? borderColor
+    : color;
+
   return (
     <svg
       width={24}
@@ -10,7 +17,7 @@ export const Color: FunctionComponent<{
       viewBox="0 0 24 24"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <circle cx={12} cy={12} r={11} fill={color} stroke={color} />
+      <circle cx={12} cy={12} r={11} fill={color} stroke={stroke} />
     </svg>
   );
 });
--- src/Tone.tsx
+++ src/Tone.tsx
@@ -1,39 +1,48 @@
 import { FunctionComponent, memo } from "react";
 import { ToneType, tonePeriod, tones } from "./tones";
 
+const displayColor = "#bdbdbd";
+const displayTargetColors = ["#fff", "#ffffff", "#f5f5f5"];
+
 export const Tone: FunctionComponent<{
   color: string;
   fuzziness: number;
   toneType: ToneType;
-}> = memo(({ color, fuzziness, toneType }) => (
-  <svg
-    width={24}
-    height={24}
-    viewBox="0 0 24 24"
-    xmlns="http://www.w3.org/2000/svg"
-  >
-    {[...Array(tonePeriod * 2).keys()].flatMap((y) =>
-      [...Array(tonePeriod * 2).keys()].map((x) => {
-        const size = 3;
+}> = memo(({ color, fuzziness, toneType }) => {
+  const toneColor = displayTargetColors.includes(color.toLowerCase())
+    ? displayColor
+    : color;
 
-        const modulatedX = Math.floor(x + Math.random() * fuzziness);
-        const modulatedY = Math.floor(y + Math.random() * fuzziness);
-        const toneBit =
-          tones[toneType].bitmap[modulatedY % tonePeriod][
-            modulatedX % tonePeriod
-          ];
+  return (
+    <svg
+      width={24}
+      height={24}
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      {[...Array(tonePeriod * 2).keys()].flatMap((y) =>
+        [...Array(tonePeriod * 2).keys()].map((x) => {
+          const size = 3;
 
-        return (
-          <rect
-            key={`${x}-${y}`}
-            x={size * x}
-            y={size * y}
-            width={size}
-            height={size}
-            fill={toneBit ? color : "#FFFFFF"}
-          />
-        );
-      })
-    )}
-  </svg>
-));
+          const modulatedX = Math.floor(x + Math.random() * fuzziness);
+          const modulatedY = Math.floor(y + Math.random() * fuzziness);
+          const toneBit =
+            tones[toneType].bitmap[modulatedY % tonePeriod][
+              modulatedX % tonePeriod
+            ];
+
+          return (
+            <rect
+              key={`${x}-${y}`}
+              x={size * x}
+              y={size * y}
+              width={size}
+              height={size}
+              fill={toneBit ? toneColor : "#FFFFFF"}
+            />
+          );
+        })
+      )}
+    </svg>
+  );
+});
